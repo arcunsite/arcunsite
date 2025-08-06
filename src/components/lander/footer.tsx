9dffeeeaa261efa1b2149e2d59b07ee0702327a1
Arman <armancurr@proton.me>
2025-08-06 10:26:35 +0530

    feat: update footer and hero components with new button and styling

--- src/components/lander/footer.tsx
+++ src/components/lander/footer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import { ArrowUpRight } from "@phosphor-icons/react";
 
 export default function Footer() {
@@ -16,22 +16,29 @@ export default function Footer() {
           className="rounded-xl border border-neutral-900 z-0"
           priority
         />
-
         <div className="absolute inset-0 flex flex-col justify-center items-center text-center p-6 sm:p-10 z-10">
+          <h2 className="text-5xl font-bold text-neutral-100 mb-4">
+            Feature Your Site
+          </h2>
+
           <p className="max-w-xl text-neutral-200 text-lg mb-6">
             If you want to contribute a portfolio or feature your own, submit it
             to our GitHub and join the community.
           </p>
 
-          <Link
-            href="https://github.com/arcunsite/arcunsite/blob/main/CONTRIBUTING.md"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-flex items-center gap-2 bg-neutral-100 hover:bg-neutral-300 transition-colors duration-200 font-medium text-neutral-950 cursor-pointer rounded-md px-3 py-2 text-sm"
+          <Button
+            onClick={() =>
+              window.open(
+                "https://github.com/arcunsite/arcunsite/blob/main/CONTRIBUTING.md",
+                "_blank",
+                "noopener,noreferrer",
+              )
+            }
+            className="bg-gradient-to-b from-neutral-100 to-neutral-400 hover:bg-neutral-300 active:scale-95 transform transition-all duration-200 text-neutral-950 cursor-pointer"
           >
-            Contribute on GitHub
             <ArrowUpRight size={18} weight="fill" />
-          </Link>
+            Contribute
+          </Button>
         </div>
       </div>
     </footer>
--- src/components/lander/hero.tsx
+++ src/components/lander/hero.tsx
@@ -18,15 +18,6 @@ export default function Hero() {
             priority
           />
           <div className="absolute inset-0 flex flex-col justify-center items-center text-center p-6 sm:p-10 z-10">
-            {/* <Button 
-                variant="secondary" 
-                size="sm" 
-                className="mb-4 bg-neutral-950 text-neutral-200 border border-neutral-700 backdrop-blur-sm"
-                disabled
-              >
-                under development!
-              </Button> */}
-
             <h1 className="text-7xl font-bold text-neutral-100 mb-4">
               Stand Out. Get Hired.
             </h1>
@@ -41,7 +32,7 @@ export default function Hero() {
               <Button
                 size="sm"
                 variant="secondary"
-                className="bg-neutral-100 hover:bg-neutral-300 transition-colors duration-200 text-neutral-950 cursor-pointer"
+                className="bg-gradient-to-b from-neutral-100 to-neutral-400 hover:bg-neutral-300 active:scale-95 transform transition-all duration-200 text-neutral-950 cursor-pointer"
               >
                 <ArrowRight size={20} weight="fill" />
                 Get Started
